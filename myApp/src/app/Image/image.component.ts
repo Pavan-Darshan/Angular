70576b6cedfc60f33847daf4a64dd5b3eab21e74
Pavan Darshan <pavan.bhuvaneshwar@itorizon.com>
2025-01-09 08:56:02 +0530

    new

---
+++ myApp/src/app/Image/image.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-image',
+    standalone:true,
+    templateUrl:'./image.component.html',
+    styleUrl : './image.component.css'
+})
+export class AppImage{}
---
+++ myApp/src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+    standalone:true,
+    templateUrl:'./header.component.html',
+    // styles : ['h1 {color:red}']
+    styleUrl:'./header.component.css'
+
+})
+
+
+
+export class HeaderComponent {}
